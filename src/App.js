baddc9e103c0bf9e3e171c55030bcd17e2a34314
Dj Carlos Hermida <djcarloshermida@outlook.com>
2024-03-26 11:35:37 -0300

    clean project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>| DJCarlos Hermida |</p>
-      </header>
+      
     </div>
   );
 }
